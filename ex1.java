02bfa6f43bfc7a7876d7453a0af07bccf26fe3e0
rakshitha2005982 <rakshhh2005@gmail.com>
2026-02-26 09:25:34 +0530

    Add files via upload

---
+++ ex1.java
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My Render Website</title>
+</head>
+<body>
+  <h1>Hello from Render Cloud 🚀</h1>
+  <p>This site is deployed from GitHub to Render.</p>
+</body>
+</html>
